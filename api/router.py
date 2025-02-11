c976b46e879096745b30cf867e5fc37258470620
Glago Gideon Elorm <glagogideonelorm2006@gmail.com>
2025-02-11 03:31:48 +0000

    ggg

--- api/router.py
+++ api/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from api.routes import books
+from api.health import router as health_router
 
 api_router = APIRouter()
 api_router.include_router(books.router, prefix="/books", tags=["books"])
+api_router.include_router(health_router)
